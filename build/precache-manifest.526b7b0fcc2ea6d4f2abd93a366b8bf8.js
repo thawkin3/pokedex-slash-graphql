62c82b3f37179d04243e7d1ff4c91026dfe90045
Tyler Hawkins <thawkin3@byu.net>
2020-08-07 15:05:17 -0600

    chore: adds production build v2

---
+++ build/precache-manifest.526b7b0fcc2ea6d4f2abd93a366b8bf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6323bde2d88e915b079b094374c68030",
+    "url": "/pokedex-slash-graphql/build/index.html"
+  },
+  {
+    "revision": "bb52bb67e56151125744",
+    "url": "/pokedex-slash-graphql/build/static/css/main.f43c30a3.chunk.css"
+  },
+  {
+    "revision": "013501d974c9f0d1f078",
+    "url": "/pokedex-slash-graphql/build/static/js/2.a795064c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex-slash-graphql/build/static/js/2.a795064c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb52bb67e56151125744",
+    "url": "/pokedex-slash-graphql/build/static/js/main.9ee43871.chunk.js"
+  },
+  {
+    "revision": "0a61b4ee318c7e6b91e7",
+    "url": "/pokedex-slash-graphql/build/static/js/runtime-main.2b2a282e.js"
+  },
+  {
+    "revision": "9253c4570f9b18eeb9d60d12c4a2a8dc",
+    "url": "/pokedex-slash-graphql/build/static/media/pokemon-logo.9253c457.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c444b0a7fe892a6093e51aa0651f1255.js"
+  "/pokedex-slash-graphql/build/precache-manifest.526b7b0fcc2ea6d4f2abd93a366b8bf8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex-slash-graphql/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
