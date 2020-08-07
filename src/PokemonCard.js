2c6174056f8a61100fd8b5c65dba501cadea6482
Tyler Hawkins <thawkin3@byu.net>
2020-08-07 14:54:20 -0600

    feat: adds switch toggle for captured state

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,10 +21,19 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 0,
     marginBottom: theme.spacing(1),
   },
+  cardActions: {
+    justifyContent: 'center',
+  },
 }))
 
 export function PokemonCard({ pokemon, imgUrl }) {
   const classes = useStyles()
+
+  const [isCaptured, setIsCaptured] = useState(false)
+
+  const handleCapturedChange = () => {
+    setIsCaptured((isCaptured) => !isCaptured)
+  }
 
   return (
     <Card className={classes.root} variant="outlined">
@@ -38,12 +49,20 @@ export function PokemonCard({ pokemon, imgUrl }) {
         <Typography variant="h5" component="h2">
           {pokemon.name}
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          {pokemon.type.join(', ')}
-        </Typography>
+        <Typography color="textSecondary">{pokemon.type.join(', ')}</Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
+      <CardActions className={classes.cardActions}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isCaptured}
+              onChange={handleCapturedChange}
+              name="captured"
+              color="primary"
+            />
+          }
+          label="Captured"
+        />
       </CardActions>
     </Card>
   )
